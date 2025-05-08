720249e2115ef83a1d9bf2a9a6ad9a859b778a5f
Evgeni Leonti <evgeni.leonti@gmail.com>
2025-05-08 12:46:40 +0300

    use matchedData

--- src/controllers/avatarController.ts
+++ src/controllers/avatarController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, matchedData } from 'express-validator';
 import { DEFAULT_AVATAR_BACKGROUND_COLOR, DEFAULT_AVATAR_RADIUS, DEFAULT_AVATAR_SIZE } from '../config/avatarConfig.js';
 import { generateAvatarSvg } from '../services/avatarService.js';
 
@@ -9,8 +9,10 @@ export const getUserAvatar = (req: Request, res: Response) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const name = req.query.name as string;
-    const bgColorInput = req.query.backgroundColor as string | undefined;
+    const { name, backgroundColor: bgColorInput } = matchedData(req, {
+        locations: ['query'],
+        includeOptionals: true
+    }) as { name: string; backgroundColor?: string };
 
     const validatedBgColor = bgColorInput ? bgColorInput.replace('#', '') : DEFAULT_AVATAR_BACKGROUND_COLOR;
 
